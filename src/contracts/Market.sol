6c9d89d25bd28494efd937a2fa2070550dc4e1db
guille-block <fairbairnguillermo@gmail.com>
2021-04-28 00:45:19 -0300

    update estado v1 | .sol

---
+++ src/contracts/Market.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.5.0;
+
+contract Market {
+    string public name;
+    uint public productCount = 0;
+    mapping(uint => Product) public products;
+
+    struct Product {
+        uint id;
+        string name;
+        address payable owner;
+        uint price;
+        bool state;
+    }
+
+    event NewProduct (
+        uint id,
+        string name,
+        address payable owner,
+        uint price,
+        bool state
+    );
+
+    constructor() public {
+        name = "G-Market";
+    }
+
+    function listProduct(string memory _name, uint _price) public {
+        require(_price>0);
+        require(bytes(_name).length>0);
+        productCount++;
+        products[productCount] = Product(productCount, _name, msg.sender, _price, false);
+        emit NewProduct(productCount, _name, msg.sender, _price, false);
+    }
+
+    function buyProduct(uint _id) public payable {
+        require(msg.value>0);
+        Product memory _product = products[_id];
+        address payable _seller = _product.owner;
+        require(_seller != msg.sender);
+        _product.owner = msg.sender;
+        _product.state = true;
+        products[_id] = _product;
+        address(_seller).transfer(msg.value);
+    }
+
+
+}
---
+++ test/Market.test.js
@@ -0,0 +1,109 @@
+const { assert } = require("chai")
+
+const Market = artifacts.require('./Market.sol')
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('Market', ([deployer, seller, buyer]) => {
+    let market 
+
+    before(async () => {
+        market = await Market.deployed()
+    })
+
+    describe('deployment', async () => {
+        it('deploys succesfully', async () => {
+            const address = await market.address
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        })
+
+        it('Has a name', async () => {
+            const name = await market.name()
+            assert.equal(name, 'G-Market')
+        })
+    })
+
+    describe('Products', async () => {
+        let count, productNew
+        let result
+
+        before(async () => {
+
+            productNew = await market.listProduct('Nokia 1100', web3.utils.toWei('1', 'Ether'), {from: seller})
+            count = await market.productCount()
+            result = productNew.logs[0].args
+        })
+
+        it('Product count Okay', async () => {
+            assert.equal(count, 1)
+            assert.equal(result.id.toNumber(), count)
+        })
+
+        it('Name product', async () => {
+            assert.equal(result.name, 'Nokia 1100')
+        })
+
+        it('Seller product', async () => {
+            assert.equal(result.owner, seller)
+        })
+
+        it('Price product', async () => {
+            assert.equal(result.price, web3.utils.toWei('1', 'Ether'))
+        })
+
+        it('State product', async () => {
+            assert.equal(result.state, false)
+        })
+
+        it('Product must have a name', async () => {
+            assert.equal(result.state, false)
+        })
+
+        it('Price must be positive', async () => {
+            await market.listProduct('Nokia 1100', web3.utils.toWei('0', 'Ether')).should.be.rejected
+        })
+
+        it('Name should exist', async () => {
+        await market.listProduct('', web3.utils.toWei('1', 'Ether')).should.be.rejected
+        })
+
+    })
+
+    describe('Buying a product', async () => {
+
+        let count, productNew, result
+
+        before(async () => {
+
+            productNew = await market.listProduct('Nokia 1100', web3.utils.toWei('1', 'Ether'), {from: seller})
+            count = await market.productCount()
+            result = productNew.logs[0].args
+        })
+
+        it('Msg sender must not be seller', async () => {
+            await market.buyProduct(1, {from: seller, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected
+        })
+
+        it('Msg value must be positive', async () => {
+            await market.buyProduct(1, {from: buyer, value: web3.utils.toWei('0', 'Ether')}).should.be.rejected
+        })
+
+        it('Correct balances', async () => {
+            let bought
+
+            let oldBalance = await web3.eth.getBalance(seller)
+            //oldBalance = await web3.utils.BN(oldBalance)
+
+            bought = await market.buyProduct(1, {from: buyer, value: web3.utils.toWei('1', 'Ether')})
+            let newBalance = await web3.eth.getBalance(seller)
+            //newBalance = await web3.utils.BN(oldBalance)
+
+           // assert.notEqual(newBalance, oldBalance , 'They are different')
+        })    
+    })
+})
