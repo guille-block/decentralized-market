06433185363a96e318b16ddd6618b027dc3f6810
guille-block <fairbairnguillermo@gmail.com>
2021-05-06 20:22:50 -0300

    Updates

---
+++ precache-manifest.0e7450253d1374ee5fb5d20284f82d60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "535632c2bdd9cf9fdc6c",
+    "url": "/decentralized-market/static/css/main.1e5c7ba0.chunk.css"
+  },
+  {
+    "revision": "535632c2bdd9cf9fdc6c",
+    "url": "/decentralized-market/static/js/main.535632c2.chunk.js"
+  },
+  {
+    "revision": "5698e26cadbd63972260",
+    "url": "/decentralized-market/static/css/1.fa200dfa.chunk.css"
+  },
+  {
+    "revision": "5698e26cadbd63972260",
+    "url": "/decentralized-market/static/js/1.5698e26c.chunk.js"
+  },
+  {
+    "revision": "076b4cf38453a5698be1",
+    "url": "/decentralized-market/static/js/runtime~main.076b4cf3.js"
+  },
+  {
+    "revision": "7edabeddf4b57954639e7a54bad4d9bb",
+    "url": "/decentralized-market/index.html"
+  }
+];
